fd15ca45d1f9724fe4516563fd1508e121967376
lcb931023 <lcb931023@gmail.com>
2022-12-11 18:22:31 +0800

    replace email collection endpoint

--- docs/src/.vuepress/theme/Downloads.vue
+++ docs/src/.vuepress/theme/Downloads.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="theme-container layout-downloads">
     <Content/>
-    <form action="https://getform.io/f/cc4ba9d8-f32f-4663-a495-0c950825c935" method="POST">
+    <form action="https://getform.io/f/53f88eb0-0b7d-478f-a276-472ae8e79d1d" method="POST">
       <label>Email: </label>
       <input type="email" name="email" required>
       <button type="submit">Submit</button>
