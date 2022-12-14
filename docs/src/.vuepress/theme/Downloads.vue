15b280bcf4487feed3b6f5e6ac2e00129446d394
lcb931023 <lcb931023@gmail.com>
2022-12-15 00:08:29 +0800

    adjust download email collection style

--- docs/src/.vuepress/theme/Downloads.vue
+++ docs/src/.vuepress/theme/Downloads.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="theme-container layout-downloads">
     <Content/>
-    <form action="https://getform.io/f/53f88eb0-0b7d-478f-a276-472ae8e79d1d" method="POST">
-      <label>Email: </label>
-      <input type="email" name="email" required>
-      <button type="submit">Submit</button>
-    </form>
   </div>
 </template>
 
-<script>
+<style lang="scss">
 
-</script>
+</style>
--- docs/src/downloads.md
+++ docs/src/downloads.md
@@ -13,3 +13,9 @@ downloads:
 ## Downloads
 
 To be informed when The Treacherous Turn is fully released, enter your email below. Thank you!
+
+<form action="https://getform.io/f/53f88eb0-0b7d-478f-a276-472ae8e79d1d" method="POST">
+  <label>Email: </label>
+  <input type="email" name="email" required>
+  <button type="submit">Submit</button>
+</form>
